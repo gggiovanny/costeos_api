b9a810085a165c9b0ada5d49e411eef948d3f3f1
gggiovanny <giovanny.gonzalez.19@gmail.com>
2021-02-10 00:49:57 -0600

    Agregado ejemplo de API mejorado.

--- api.py
+++ api.py
@@ -1,20 +1,23 @@
-import enum
-from flask import Flask
+from flask import request, url_for
+from flask_api import FlaskAPI, status, exceptions
 from models_db import *
-app = Flask(__name__)
+from pony.flask import Pony
+
+app = FlaskAPI(__name__)
+Pony(app) # para usar db_session automaticamente en las peticiones
 connect()
 
-@app.route('/')
-def hello_world():
-    return '¡Bienvenid@ a la API de costeos!'
+@app.route("/", methods=['GET'])
+def raiz():
+    return {'bienvenida': '¡Bienvenid@ a la API de costeos!'}
 
-@app.route('/costosfijos')
+@app.route('/costosfijos',  methods=['GET'])
 def get_costos_fijos():
-    with db_session:
-        res = {}
-        costos = CostosFijos.select()[:]
-        
-        for val in costos:
-            res['{}'.format(val.id)] = val.to_dict()
-        return res
-    
+    return [c.to_dict() for c in CostosFijos.select()]
+
+@app.route("/costosfijos/<int:id>/", methods=['GET'])
+def get_costo_fijo(id):
+    return CostosFijos[id].to_dict()
+
+if __name__ == "__main__":
+    app.run(debug=True)
