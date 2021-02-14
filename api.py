38a72cea796f297ff3f131e295c2d00f1510ae23
gggiovanny <giovanny.gonzalez.19@gmail.com>
2021-02-13 18:15:56 -0600

    Agregados mas endpoints de get

--- api.py
+++ api.py
@@ -19,5 +19,24 @@ def get_costos_fijos():
 def get_costo_fijo(id):
     return CostosFijos[id].to_dict()
 
+@app.route('/unidades',  methods=['GET'])
+def get_unidades():
+    return [c.to_dict() for c in Unidades.select()]
+
+@app.route("/unidades/<int:id>/", methods=['GET'])
+def get_unidad(id):
+    return Unidades[id].to_dict()
+
+@app.route('/insumos',  methods=['GET'])
+def get_insumos():
+    return [c.to_dict() for c in Insumos.select()]
+
+@app.route("/insumos/<int:id>/", methods=['GET'])
+def get_insumo(id):
+    return Insumos[id].to_dict()
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
