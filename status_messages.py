64135289a25be69285614ca5ba47dacdc91d0646
gggiovanny <giovanny.gonzalez.19@gmail.com>
2021-02-27 23:18:38 -0600

    Agregados nuevos mensajes de status

--- status_messages.py
+++ status_messages.py
@@ -2,15 +2,17 @@ from pydantic import BaseModel
 from pony.orm.core import Entity
 
 
-def notFoundMsg(fieldvalue: str, objectname: str = None, fieldname: str = "id"):
-    if not objectname:
-        return "No existe un registro con {}: {}".format(
-            fieldname,
-            fieldvalue
-        )
-    else:
-        return "No existe un registro de {} con {}: {}".format(
-            objectname,
-            fieldname,
-            fieldvalue
-        )
+def notFoundMsg(fieldvalue: str, objectname: str = "registro", fieldname: str = "id"):
+    return "No existe un {} con {}: {}".format(
+        objectname,
+        fieldname,
+        fieldvalue
+    )
+
+
+def duplicatedMsg(fieldvalue: str, objectname: str = "registro", fieldname: str = "id"):
+    return "Ya existe un {} con {}: {}".format(
+        objectname,
+        fieldname,
+        fieldvalue
+    )
