70cf2a9bc6ec0ffaf7c2078cb56b01d3e0d81cc2
gggiovanny <giovanny.gonzalez.19@gmail.com>
2021-03-04 00:39:11 -0600

    Agregado CORS de deploy en github pages.

--- api.py
+++ api.py
@@ -16,6 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost",
         "http://localhost:3000",
+        "https://gggiovanny.github.io/costeos_app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
