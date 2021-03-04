c6fd9bc7b79acb0ab7581da7bdebc7ac1cc2bbd6
gggiovanny <giovanny.gonzalez.19@gmail.com>
2021-03-04 00:41:33 -0600

    Corregido endpoint de github pages

--- api.py
+++ api.py
@@ -16,7 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost",
         "http://localhost:3000",
-        "https://gggiovanny.github.io/costeos_app",
+        "https://gggiovanny.github.io",
     ],
     allow_credentials=True,
     allow_methods=["*"],
