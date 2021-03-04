2408ff0b40ddbbc1e50fa624d67364b8685cd796
gggiovanny <giovanny.gonzalez.19@gmail.com>
2021-03-04 01:42:34 -0600

    Agregado código para intentar que la api se conecte a heroku postgres.

--- database.py
+++ database.py
@@ -1,12 +1,22 @@
 from pony.orm import *
+import os
+
+# obteniendo la url de la base de datos definida como varible del sistema
+DATABASE_URL = os.environ['DATABASE_URL']
+
 # creando el objeto de la base de datos
 db = Database()
 
 
-def connect():
+def connect(use_sqlite=False):
     # conectando a la base de datos
-    db.bind(provider='sqlite', filename='cache.sqlite',
-            create_db=True)  # file database
+    if use_sqlite:
+        db.bind(provider='sqlite', filename='cache.sqlite',
+                create_db=True)
+    else:
+        db.bind(provider='postgres', user='', password='', host='',
+                database=DATABASE_URL, sslmode='require')
+
     # debug mode: show querys
     set_sql_debug(True)
     # mapeando modelos a la base de datos
